9540287675b107a999293c1863267ec83f931524
DOMINIQUE SOTO <dominique.soto@uc.cl>
2024-06-26 22:46:36 -0400

    fix(links)

--- src/common/main.jsx
+++ src/common/main.jsx
@@ -4,12 +4,15 @@ import './index.css'
 import Routing from './Routing'
 import AuthProvider from '../auth/AuthProvider'
 import WaitProvider from '../wait/WaitProvider'
+import FightProvider from '../fight/FightProvider'
 
 ReactDOM.createRoot(document.getElementById('root')).render(  
   <React.StrictMode>
     <AuthProvider>
       <WaitProvider>
-        <Routing />
+        <FightProvider>
+          <Routing />
+        </FightProvider>
       </WaitProvider>
     </AuthProvider>
   </React.StrictMode>,
